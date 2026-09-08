3baa886816324794877325ef3102fb1f04c19e01
yousef <yousef@gmail.com>
2026-09-09 02:04:45 +0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [react()],
+  base: '/cineverse/', 
 })
