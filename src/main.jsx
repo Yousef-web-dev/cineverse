5acd92f901cdf2fdb8186ff2ecd812c074de2b49
yousef <yousef@gmail.com>
2026-09-22 07:06:03 +0300

    update project files

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+<BrowserRouter basename={import.meta.env.DEV ? "" : "/cineverse"}>
+  <App />
+</BrowserRouter>
   </React.StrictMode>
 );
